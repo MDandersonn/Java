cf100f3b090d7280c0e727ffdce42c9afbb8d41c
MDandersonn <kimyj1215@naver.com>
2023-02-28 08:03:54 +0900

    마이바티스

---
+++ KH_Mybatis/src/main/java/kr/co/vo/AccountVO.java
@@ -0,0 +1,94 @@
+package kr.co.vo;
+
+import java.sql.Date;
+import java.util.Objects;
+public class AccountVO {
+	
+	private int id; // ID NUMBER PRIMARY KEY
+	private String nickname; // NICKNAME VARCHAR2(35) UNIQUE
+	private String email; // EMAIL VARCHAR2(100) UNIQUE
+	private char isAllow; // ISALLOW CHAR(1) DEFAULT('N') CHECK (ISALLOW IN ('N', 'Y'))
+	private Date reqDate; // REQDATE DATE DEFAULT(SYSDATE)
+	private String password;
+	private Date loginDate;
+	
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Date getLoginDate() {
+		return loginDate;
+	}
+
+	public void setLoginDate(Date loginDate) {
+		this.loginDate = loginDate;
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getNickname() {
+		return nickname;
+	}
+	
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public char getIsAllow() {
+		return isAllow;
+	}
+	
+	public void setIsAllow(char isAllow) {
+		this.isAllow = isAllow;
+	}
+	
+	public Date getReqDate() {
+		return reqDate;
+	}
+	
+	public void setReqDate(Date reqDate) {
+		this.reqDate = reqDate;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AccountVO other = (AccountVO) obj;
+		return id == other.id;
+	}
+	
+	@Override
+	public String toString() {
+		return "AccountVO [id=" + id + ", nickname=" + nickname + ", email=" + email + ", isAllow=" + isAllow
+				+ ", reqDate=" + reqDate + ", password=" + password + ", loginDate=" + loginDate + "]";
+	}
+	
+}
