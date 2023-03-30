ec82e3d37fa80068da078e264d6c6c800249f354
MDandersonn <kimyj1215@naver.com>
2023-03-31 02:16:11 +0900

    test: [김영진] array 출력테스트

---
+++ Test/src/interfacePrac/ArrayTest.java
@@ -0,0 +1,19 @@
+package interfacePrac;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayTest {
+
+	public static void main(String[] args) {
+		List<Integer> list = new ArrayList <Integer> ();
+		list.add(1);
+		list.add(3);
+		list.add(5);
+		System.out.println(list);
+		
+
+	}
+
+}
